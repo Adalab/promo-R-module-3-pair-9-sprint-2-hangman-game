a6b434adaf46f47c8477872ba9fc88632710dd71
Blanca Ubis <ubis.blanca@gmail.com>
2022-11-29 11:27:18 +0100

    Add exercises 3.6: 1, 2, 3, 4

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,13 @@
 import { useEffect, useState } from 'react';
-
 // api
 import getWordFromApi from '../services/api';
 // styles
 import '../styles/App.scss';
-import '../styles/Dummy.scss';
-import '../styles/Letters.scss';
 import '../styles/Form.scss';
-import '../styles/Header.scss';
+//components
+import Header from './Header';
+import Dummy from './Dummy';
+import SolutionLetters from './SolutionLetters';
 
 function App() {
   const [word, setWord] = useState('');
@@ -28,7 +28,7 @@ function App() {
   };
 
   const handleChange = (ev) => {
-    let re = /^[a-zA-ZñÑá-úÁ-Ú´]$/; //add regular pattern 
+    let re = /^[a-zA-ZñÑá-úÁ-Ú´]$/; //add regular pattern
     if (re.test(ev.target.value) || ev.target.value === '') {
       handleLastLetter(ev.target.value);
     }
@@ -45,18 +45,6 @@ function App() {
     return errorLetters.length;
   };
 
-  const renderSolutionLetters = () => {
-    const wordLetters = word.split('');
-    return wordLetters.map((letter, index) => {
-      const exists = userLetters.includes(letter.toLocaleLowerCase());
-      return (
-        <li key={index} className='letter'>
-          {exists ? letter : ''}
-        </li>
-      );
-    });
-  };
-
   const renderErrorLetters = () => {
     const errorLetters = userLetters.filter(
       (letter) =>
@@ -64,7 +52,7 @@ function App() {
     );
     return errorLetters.map((letter, index) => {
       return (
-        <li key={index} className='letter'>
+        <li key={index} className="letter">
           {letter}
         </li>
       );
@@ -82,53 +70,34 @@ function App() {
   };
 
   return (
-    <div className='page'>
-      <header>
-        <h1 className='header__title'>Juego del ahorcado</h1>
-      </header>
-      <main className='main'>
+    <div className="page">
+      <Header />
+      <main className="main">
         <section>
-          <div className='solution'>
-            <h2 className='title'>Solución:</h2>
-            <ul className='letters'>{renderSolutionLetters()}</ul>
+          <SolutionLetters word={word} userLetters={userLetters} />
+          <div className="error">
+            <h2 className="title">Letras falladas:</h2>
+            <ul className="letters">{renderErrorLetters()}</ul>
           </div>
-          <div className='error'>
-            <h2 className='title'>Letras falladas:</h2>
-            <ul className='letters'>{renderErrorLetters()}</ul>
-          </div>
-          <form className='form' onSubmit={handleSubmit}>
-            <label className='title' htmlFor='last-letter'>
+          <form className="form" onSubmit={handleSubmit}>
+            <label className="title" htmlFor="last-letter">
               Escribe una letra:
             </label>
             <input
               autoFocus
-              autoComplete='off'
-              className='form__input'
-              maxLength='1'
-              type='text'
-              name='last-letter'
-              id='last-letter'
+              autoComplete="off"
+              className="form__input"
+              maxLength="1"
+              type="text"
+              name="last-letter"
+              id="last-letter"
               value={lastLetter}
               onKeyDown={handleKeyDown}
               onChange={handleChange}
             />
           </form>
         </section>
-        <section className={`dummy error-${getNumberOfErrors()}`}>
-          <span className='error-13 eye'></span>
-          <span className='error-12 eye'></span>
-          <span className='error-11 line'></span>
-          <span className='error-10 line'></span>
-          <span className='error-9  line'></span>
-          <span className='error-8  line'></span>
-          <span className='error-7  line'></span>
-          <span className='error-6  head'></span>
-          <span className='error-5  line'></span>
-          <span className='error-4  line'></span>
-          <span className='error-3  line'></span>
-          <span className='error-2  line'></span>
-          <span className='error-1  line'></span>
-        </section>
+        <Dummy numberOfErrors={getNumberOfErrors()} />
       </main>
     </div>
   );
